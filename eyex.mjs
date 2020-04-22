6b3d00d37918ed5da77f1cf009e3a4f27f2a9ee2
Mostafa Elshamy <melhosseiny@users.noreply.github.com>
2020-04-22 12:00:05 +0200

    Initial commit

---
+++ eyex.mjs
@@ -0,0 +1,21 @@
+import SocketIO from 'socket.io';
+import edge from 'edge-js';
+
+const PORT = process.env.PORT || 80;
+
+let io = SocketIO(PORT);
+
+let EyeXFramework = edge.func('eyex.cs');
+
+let payload = {
+  x: 'random',
+  onGazePoint: function(data, callback) {
+    io.emit('news', data)
+    callback(null, console.log(data))
+  }
+}
+
+EyeXFramework(payload, function (error, result) {
+    if (error) throw error;
+    console.log(result);
+});
